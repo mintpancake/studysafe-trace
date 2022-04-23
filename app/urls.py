88ed9304c9636215ff38a9604b80db37729652ba
AQing-527 <54972646+AQing-527@users.noreply.github.com>
2022-04-23 12:19:59 +0800

    implement two apis: /venues/ and /contacts/

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from app import views
 
-urlpatterns = []
+urlpatterns = [
+    path('venues/<str:hku_id>/<str:date>', views.InfectiousVenues.as_view(), name="infectious_venues"),
+    path('contacts/<str:hku_id>/<str:date>', views.CloseContacts.as_view(), name="close_contacts")
+]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+import requests
+import json
+
 
 # Create your views here.
+class InfectiousVenues(APIView):
+
+    def get(self, request, hku_id, date, format=None):
+
+        url = "http://blooming-beach-58892.herokuapp.com/api/trace/visits/" + hku_id + "/" + date
+        response = requests.get(url)
+        result = response.json()
+
+        if response.status_code == 200:
+            venues = []
+            for i in range(len(result)):
+                if result[i]["venue"] not in venues:
+                    venues.append(result[i]["venue"])
+
+            venues.sort()
+            context = {"venues": venues,
+                       "subject": hku_id,
+                       "date": date}
+            return render(request, 'venues.html', context=context)
+
+
+class CloseContacts(APIView):
+
+    def get(self, request, hku_id, date, format=None):
+
+        url = "http://blooming-beach-58892.herokuapp.com/api/trace/contacts/" + hku_id + "/" + date
+        response = requests.get(url)
+        result = response.json()
+
+        if response.status_code == 200:
+            contacts = []
+            for i in range(len(result)):
+                if result[i]["hku_id"] not in contacts:
+                    contacts.append(result[i]["hku_id"])
+
+            contacts.sort()
+            context = {"contacts": contacts,
+                       "subject": hku_id,
+                       "date": date
+                       }
+            return render(request, 'contacts.html', context=context)
